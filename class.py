67f50b50c43685e64a74ac436b2ef80500146a55
45-1-SA <43290721+45-1-SA@users.noreply.github.com>
2018-09-22 08:43:52 +0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,16 @@
+class table(object):
+   
+ 
+    def __init__(self, color, doors, tires):
+        self.name = name
+        self.user_name = user_name 
+        self.props = props
+    
+
+    
+    def print(self):
+        return "print"
+ 
+if __name__ == "__main__":
+    a = table("tt", 5, 4)
+    print(a.name)
